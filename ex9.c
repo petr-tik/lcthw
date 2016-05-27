4673ebc444216eaa4e39bc96cce1e94749c3aa5c
petr-tik <petr-tik@users.noreply.github.com>
2016-05-27 23:03:29 +0100

    did some extra exercises

--- ex9.c
+++ ex9.c
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[])
 {
     int numbers[4] = {0};
-    char name[5] = {'a'};
+    char *name = "\0\0a\0\0";
     
     // first, print them out raw
     printf("numbers: %d, %d, %d, %d\n", numbers[0], numbers[1],
@@ -26,30 +26,28 @@ int main(int argc, char *argv[])
     name[2] = 't';
     name[3] = 'r';
     name[4] = '\0';
-    printf("##############\nName all set up now\n");   
-
+    printf("##############\n\nName all set up now\n\n");   
 
     // then print them out initialised
     printf("numbers: %d, %d, %d, %d\n", numbers[0], numbers[1], numbers[2],
             numbers[3]);
-    printf("name each: %c %c %c %c %c\n", name[0], name[1], name[2], name[3],
-            name[4]);
-
+    printf("name by character: %c %c %c %c %c\n", name[0], name[1], 
+                    name[2], name[3], name[4]);
+    printf("%d\n", name);
     //print them like a string
     printf("name: %s\n", name);
 
     // another way to use name - more common to assign values to strings
+    // use this - this is an idiomatic C convention
     char *another = "Petr";
     
     printf("another:%s\n", another);
 
-    printf("another each: %c %c %c %c\n", another[0], another[1], another[2],
+    printf("another by character: %c %c %c %c\n", another[0], another[1], another[2],
             another[3]);
 
 
 // doing extras
 
-
     return 0;
-
 }
