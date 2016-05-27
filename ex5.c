e7265c6cc3d4e69ffb56c33af33f24f7946fd2d1
petr-tik <petr-tik@users.noreply.github.com>
2016-05-27 21:06:59 +0100

    refreshed

--- ex5.c
+++ ex5.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-/* this is a comment */
+/* this is a comment 
+with as many lines as you need*/
 int main(int argc, char *argv[])
 {
     int distance = 100;
+
     // this is also a comment
     printf("You are %d miles away\n", distance);
 
     return 0;
 }
-
-
