409205216626152ddfd7829da7674d935c900daa
petr-tik <petr-tik@users.noreply.github.com>
2016-05-27 21:40:16 +0100

    playing around with chars

--- ex7.c
+++ ex7.c
@@ -6,9 +6,11 @@ int main(int argc, char *argv[])
     double bug_rate = 1.2;
     printf("You have %d bugs at the imaginary rate of %f.\n", bugs, bug_rate);
 
+    // original
+    //  long universe_of_defects = 1L * 1024L * 1024L * 1024L;
     unsigned long universe_of_defects = 1L * 1024L * 1024L * 1024L* 1024L * 1024L * 5024L;
     printf("The entire universe has %ld bugs.\n", universe_of_defects);
-
+    // printf("%ld"); - prints as long decimal
     double expected_bugs = bugs * bug_rate;
     printf("You are expected to have %f bugs. \n", expected_bugs);
 
@@ -23,6 +25,19 @@ int main(int argc, char *argv[])
     int care_percentage = bugs * nul_byte;
     printf("Which means you should care %d%%.\n", care_percentage);
 // % is an escape char for %, so if you want to print 5%, you need to printf("5%%")
+    
+    char a = 'a';
+    char b = 'b';
+    char A = 'A';
+    char B = 'B';
+    int num = 5;
+    int res = num * a;
+    printf("a as char is %c and as num is %d\n", a, a);
+    printf("b as char is %c and as num is %d\n", b, b);
+    printf("A as char is %c and as num is %d\n", A, A);
+    printf("When you multiply %c by %d = %d\n", a, num, res);
+    printf("%c * %d = %d \n", b, num, b * num);
+    // printing the decimal value of char gives you the ascii code
     return 0;
 
 }
