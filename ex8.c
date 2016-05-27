d7dfec00945dff690223668d8543a1b47131690f
petr-tik <petr-tik@users.noreply.github.com>
2016-05-27 22:51:12 +0100

    reassigned individual chars in string

--- ex8.c
+++ ex8.c
@@ -2,29 +2,26 @@
 
 int main(int argc, char *argv[])
 {
+    char full_name[] = {
+    'P', 'e', 't', 'r', ' ', 
+    'A', '.', ' ', 
+    'T', 'i', 'k','i', 'l', 'y', 'a', 'y', 'n', 'e', 'n', '\0'
+    };
     int areas[] = {10, 12, 13, 14, 20};
     char name[] = "Petr";
-    char full_name[] = {
-	'P', 'e', 't', 'r', ' ', 
-	'A', '.', ' ', 
-	'T', 'i', 'k','i', 'l', 'y', 'a', 'y', 'n', 'e', 'n', '\0'
-    };
-
+    // name[2] = 'A';
+    
     // WARNING: ons some systems you may have to change the %ld
     // to a %u since it will use UNsigned bits
     printf("The size of an int: %ld\n", sizeof(int));
     printf("The size of areas (int[]): %ld\n", sizeof(areas));
     printf("The first area is %d, the 2nd %d.\n", areas[0], areas[1]);
-    
     printf("The size of a char: %ld\n", sizeof(char));
     printf("The size of name (char[]): %ld\n", sizeof(name));
-    printf("The number of charas: %ld\n", sizeof(name)/sizeof(char));
-    
+    printf("The number of chars: %ld\n", sizeof(name)/sizeof(char));
     printf("The size of full_name (char[]): %ld\n", sizeof(full_name));
     printf("The number of chars: %ld\n", sizeof(full_name)/sizeof(char));
+    printf("name = \"%s\" and full_name=\"%s\"\n", name, full_name); 
 
-    printf("name\"%s\" and full_name=\"%s\"\n", name, full_name); 
-
-    
     return 0;
 }
