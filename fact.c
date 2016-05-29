2ae18109a5dc6f11c5b57a8f4c2509e90d023de0
petr-tik <petr-tik@users.noreply.github.com>
2016-05-29 17:25:25 +0100

    added comments and check variable

--- fact.c
+++ fact.c
@@ -14,10 +14,12 @@ int factorial(int upper){
 int main(int argc, char *argv[])
 {
     int var = 0;
-    fscanf(stdin, "%d", &var);
+    // fscanf(stdin, "%d", &var);
     // fscnaf(stdin, conversion format, pointer to intitialised variable of that type)
-
-    // 
+    // if fscanf works fine it returns 1; else return 0;
+    int input_check;
+    input_check = fscanf(stdin, "%d", &var);
+    // check(input_check > 0, "Enter a valid number");
     printf("The factorial of %d is %d\n", var, factorial(var));
     return 0;
 }
