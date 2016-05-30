308a6ad60c6ffc19de768ad5a3f55eaa108258bd
petr-tik <petr-tik@users.noreply.github.com>
2016-05-30 13:19:20 +0100

    wrote the exercise down

---
+++ ex14.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <ctype.h>
+
+// forward declaration
+int can_print_it(char ch); // a func that takes a char and returns int
+void print_letters(char arg[]); // func takes char array (string), returns nothing
+
+void print_arguments(int argc, char *argv[])
+{
+    int i = 0;
+
+    for(i = 0; i < argc; i++)
+    {
+        print_letters(argv[i]);
+    }
+}
+
+void print_letters(char arg[]){
+    int i = 0;
+    for(i = 0; arg[i] != '\0'; i++){
+        char ch = arg[i];
+        if(can_print_it(ch)) {
+            printf("'%c' == %d in ascii ", ch, ch);
+        }
+    }
+    printf("\n");
+}
+
+int can_print_it(char ch)
+{
+    return isalpha(ch) || isblank(ch);
+}
+
+int main(int argc, char *argv[])
+{
+    print_arguments(argc, argv);
+    return 0;
+}
