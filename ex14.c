cee832613353a82fde3b74de16fd8fdbdc9b9e8e
petr-tik <petr-tik@users.noreply.github.com>
2016-05-31 21:09:18 +0100

    implemented a for loop using strlen

--- ex14.c
+++ ex14.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 // forward declaration
-int can_print_it(char ch); // a func that takes a char and returns int
-void print_letters(char arg[]); // func takes char array (string), returns nothing
+// int can_print_it(char ch); // a func that takes a char and returns int
+void print_letters(int length, char arg[]); // func takes char array (string), returns nothing
 
 void print_arguments(int argc, char *argv[])
 {
     // int i = 0;
-    for(int i = 0; i < argc; i++)
+    for(int i = 1; i < argc; i++)
     {
-        print_letters(argv[i]);
+        int length = strlen(argv[i]);
+        print_letters(length, argv[i]);
     }
 }
 
-void print_letters(char arg[]){
+void print_letters(int length, char arg[]){
     int i = 0;
-    for(i = 0; arg[i] != '\0'; i++){
+    for(i = 0; i < length; i++){
         char ch = arg[i];
         if(isalpha(ch) || isblank(ch)) {
             printf("'%c' == %d in ascii ", ch, ch);
