ec6b41e6955f7bc20a50f9ec02d569e1cbcff9d5
petr-tik <petr-tik@users.noreply.github.com>
2016-06-02 21:25:50 +0100

    wrote down the problem statement

---
+++ ex16.c
@@ -0,0 +1,67 @@
+#include <stdio.h> 
+#include <assert.h> 
+#include <stdlib.h> 
+#include <string.h>
+
+struct Person {
+    char *name;
+    int age;
+    int height;
+    int weight;
+};
+
+struct Person *Person_create(char *name, int age, int height, int weight) {
+    struct Person *who = malloc(sizeof(struct Person));
+    assert(who != NULL);
+
+    who->name = strdup(name);
+    who->age = age;
+    who->height = height;
+    who->weight = weight;
+
+    return who;
+}
+
+void Person_destroy(struct Person *who){
+    assert(who != NULL);
+
+    free(who->name);
+    free(who);
+}
+
+void Person_print(struct Person *who) {
+    printf("Name: %s\n", who->name);
+    printf("Age: %d\n", who->age);
+    printf("Height: %d\n", who->height);
+    printf("Weight: %d\n", who->weight);
+}
+
+int main(int argc, char *argv[])
+{
+    // make 2 people
+    struct Person *joe = Person_create("Joe Alex", 32, 64, 140);
+    struct Person *frank = Person_create("Frank Underwood", 20, 72, 180);
+
+    // print them out and their memory locations
+    printf("Joe's memory address is: %p\n", joe);
+    Person_print(joe);
+
+    printf("Frank's memory address is: %p\n", frank);
+    Person_print(frank);
+
+    // make everyone 20 years older and print again
+    joe->age += 20;
+    joe->height -= 2; //became shorter too
+    joe->weight += 40;
+    Person_print(joe);
+
+    frank->age += 20;
+    frank->weight += 20;
+    Person_print(frank);
+
+    // destroy them both to leave it clean
+    Person_destroy(joe);
+    Person_destroy(frank);
+
+    return 0;
+}
