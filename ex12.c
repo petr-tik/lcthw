7bf52a96970565d786a8718223fc915d6a0a8adf
petr-tik <petr-tik@users.noreply.github.com>
2016-06-02 22:55:08 +0100

    typo

--- ex12.c
+++ ex12.c
@@ -8,15 +8,13 @@ int main(int argc, char *argv[])
 		printf("You have no arguments - that's boring\n");
 	} else if(argc > 1 && argc < 4){
 		printf("here are your arguments:\n");
-
 		for(i = 1; i <= argc; i++){
 			printf("%s\n", argv[i]);
 		} 
 		printf("\n~~~~~~~~~~~\n");
 	} else {
-		printf("You have more than 4 arguemnts. I can't handle the truth! \n");
+		printf("You have more than 4 arguments. I can't handle the truth! \n");
 
 	}
-
 	return 0;
 }
