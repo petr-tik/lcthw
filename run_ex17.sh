61c2fd2510142af53c0442d6ac086f05cb0991f6
petr-tik <petr-tik@users.noreply.github.com>
2016-06-04 23:46:17 +0100

    added valgrind runs

--- run_ex17.sh
+++ run_ex17.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -x
 ./ex17 db.dat c
+stat db.dat | grep "Size"
 ./ex17 db.dat s 1 zed zed@zedshaw.com
+stat db.dat | grep "Size"
 ./ex17 db.dat s 2 frank frank@zedshaw.com
+stat db.dat | grep "Size"
 ./ex17 db.dat s 3 joe joe@zedshaw.com
-./ex17 db.dat s 4 Petr lad@gmail.com
+valgrind ./ex17 db.dat s 4 Petr lad@gmail.com
 ./ex17 db.dat l
 ./ex17 db.dat d 3
 ./ex17 db.dat l
