dd44179500393e537fc09bdee720bb83c2a754d2
petr-tik <petr-tik@users.noreply.github.com>
2016-07-12 22:51:10 +0100

    simulate nCr func - start of nCr

--- euler53.c
+++ euler53.c
@@ -10,6 +10,12 @@ make a factorial function
 make a nCr function 
 brute force through the options
 
+Compile:
+gcc -lm euler53.c -o euler53
+
+Run: 
+./euler53 <N_factorial>
+
 */
 
 int fact(unsigned int *num_ptr, long *fact_of_number)
@@ -25,13 +31,38 @@ int fact(unsigned int *num_ptr, long *fact_of_number)
         printf("Error - negative numbers don't have a factorial\n");
         return 1;
     }
-    for (int i = 2; i < number + 1; i++)
+    for (int i = 2; i <= number; i++)
     {
         *fact_of_number *= i;
     }
     return 0;
 }
 
+int nCr(int *N_ptr, int r, long *res_ptr)
+{
+    /* Takes pointer to N and r
+    return 0 if number successfully assigned to res_ptr
+    */
+    return 0;
+
+}
+
+
+int simulate_nCr(int N, int upper_limit) {
+    /* Given N - top number, return the number of nCr values above the upper_limit */
+    int counter = 0;
+    long *nCr_value_ptr = malloc(sizeof(long));
+    for (int num = 0; num <= N; num++)
+    {
+        for (int r = 0; r < num; r++) {
+            nCr(num, r, nCr_value_ptr)
+            if (*nCr_value_ptr > upper_limit) {
+                counter += 1;
+            }
+        }
+    }
+    return counter;
+}
 
 int main(int argc, char *argv[])
 {
