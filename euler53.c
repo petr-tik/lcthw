589838150797216586fbe7153de34a68b62dcced
petr-tik <petr-tik@users.noreply.github.com>
2016-07-13 21:40:56 +0100

    changed structure

--- euler53.c
+++ euler53.c
@@ -18,35 +18,40 @@ Run:
 
 */
 
-int fact(unsigned int *num_ptr, long *fact_of_number)
+int fact(unsigned int num, long *fact_of_number)
 {
     /*Takes:
     *       pointer to a number 
             pointer to factorial of number
     *   return 0 if all goes well
     */
-    int number = *num_ptr;
-    if ((number) < 0)
+    
+    if ((num) < 0)
     {
         printf("Error - negative numbers don't have a factorial\n");
         return 1;
     }
-    for (int i = 2; i <= number; i++)
+    for (int i = 2; i <= num; i++)
     {
         *fact_of_number *= i;
     }
     return 0;
 }
 
-int nCr(int *N_ptr, int r, long *res_ptr)
+int nCr(int N, int r, long *res_ptr)
 {
     /* Takes pointer to N and r
     return 0 if number successfully assigned to res_ptr
     */
+    // return f(n)/(f(r)*f(n-r))
+    long *n_fact, *r_fact, *n_r_fact;
+    fact(N, n_fact);
+    fact(r, r_fact);
+    fact(N - r, n_r_fact);
+    *res_ptr = *n_fact/((*r_fact)*(*n_r_fact));
+
     return 0;
-
 }
-
 
 int simulate_nCr(int N, int upper_limit) {
     /* Given N - top number, return the number of nCr values above the upper_limit */
@@ -55,25 +60,22 @@ int simulate_nCr(int N, int upper_limit) {
     for (int num = 0; num <= N; num++)
     {
         for (int r = 0; r < num; r++) {
-            nCr(num, r, nCr_value_ptr)
+            nCr(num, r, nCr_value_ptr);
             if (*nCr_value_ptr > upper_limit) {
                 counter += 1;
             }
         }
     }
+    free(nCr_value_ptr);
     return counter;
 }
 
 int main(int argc, char *argv[])
 {
-    unsigned int number = atoi(argv[1]);
-    unsigned int *num_ptr = &number;
-    long fact_of_number = 1;
-    long *ptr_to_fact = &fact_of_number;
-    int errno = fact(num_ptr, ptr_to_fact);
-    if (errno == 0)
-    {
-        printf("%ld\n", *ptr_to_fact);
-    } 
+    int N = 100, res;
+    int upper_limit = 1000000;
+    res = simulate_nCr(N, upper_limit);
+    
+    printf("%d\n", res); 
     return 0;
 }
