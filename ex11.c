4fb840863309c28bf521150378596f3f4355105e
petr-tik <petr-tik@users.noreply.github.com>
2016-07-16 22:53:42 +0100

    new helper print func for states
    still stack smashing

--- ex11.c
+++ ex11.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+void print_states(char *states[], int num_states) {
+  int idx = 0;
+  while(idx < num_states) {
+    printf("State %d: %s\n", idx, states[idx]);
+    idx++;
+  }
+}
+
 int main(int argc, char *argv[])
 {
 // go through each string in argv
@@ -9,28 +17,25 @@ int main(int argc, char *argv[])
 		"California", "Oregon", "Washington", 
 		"Texas"
 	};
-	int start_num_states = 4;
-	int num_states = 4 + argc - 1;
 
+	int start_num_states = 4;
+	int new_states = start_num_states + argc - 1;
 	int i = argc;
+
 	if(argc == 1){
 		printf("I don't need to copy anything - great. \n");
+                print_states(states, new_states);
 	}
 	
 	// extra credit - use a while loop to copy argv into states
 	while(i > 1){
 		printf("copying arg %d: %s into states\n", i, argv[i - 1]);
-		
-		states[start_num_states + i - 2] = argv[i - 1];
-		printf("%d/%d\n", start_num_states + i - 2, num_states - 1);
+                states[new_states - i + 1] = argv[i - 1];
+		printf("%d/%d\n", new_states - i, i - 1);
 		i--;
 	};
 
-	int y = 0; // watch out
-	while(y < num_states){
-		printf("state %d: %s\n", y, states[y]);
-		y++;
-	};	
+        print_states(states, new_states);
 	return 0; //why not just make a void function?
 };
 
