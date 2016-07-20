0043359e76168deefc33eaba71073444789476f4
petr-tik <petr-tik@users.noreply.github.com>
2016-07-20 22:31:07 +0100

    Update palindromes.c

--- palindromes.c
+++ palindromes.c
@@ -4,25 +4,21 @@
 int is_palindrome(char *word) {
     /* Given a pointer to a string, return 1 if palindrome, 0 otherwise*/ 
     int length = sizeof(char)*strlen(word);
-    for (int idx = 0; idx < length/2; idx++)
-    {
+    for (int idx = 0; idx < length/2; idx++) {
         char left_char = word[idx];
         char right_char = word[length - 1 - idx];
         if (left_char != right_char)
         {
             return 0;
         }
-
-    }
+}
     return 1;
 }
 
 int smallest_palindrome(char *word) {
     /* Given a long string (very long) 
     return the length of the smallest substring-palindrome
-    
     */
-
     int length = sizeof(char)*strlen(word);
     int res = 0;
 
@@ -32,8 +28,7 @@ int smallest_palindrome(char *word) {
         char cur = word[idx];
         char next = word[idx + 1];
 
-        if (prev == cur || cur == next) 
-        {
+        if (prev == cur || cur == next) {
             res = 2;
             return res;
         } else if (prev == next) {
