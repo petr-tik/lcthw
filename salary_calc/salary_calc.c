38ab7d78b6b6b1e51d013a5ea00f5e26bdf95575
petr-tik <petr-tik@users.noreply.github.com>
2016-07-28 22:13:22 +0100

    works for british taxes - Basic version.

--- salary_calc/salary_calc.c
+++ salary_calc/salary_calc.c
@@ -1,42 +1,73 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdio.h>
+#include <float.h>
 
-// to run 
-// gcc salary_calc.c -lm -o salary_calc
-// ./salary_calc
+/*
+to run 
+gcc salary_calc.c -lm -o salary_calc
+EITHER
 
+./salary_calc <amount>
 
-/* 
+OR
+
+./salary_calc
+What are you offered?
+<amount>
+ 
 salary | tax rate
 11000  | 0%
 43000  | 20%
-80000  | 30%
-100000 | 40%
+43000  | 40%
 150000 | 45%
-
 
 if you earn 100,000, you pay
 40% * (100,000 - 80,000) + 30% * (80,000 - 43,000) + 20%(43,000 - 11,000) = 25,500 GBP in taxes
-
-
-
 */
 
-int tax_from_sum(float *salary, float *tax_rate, float *taxes_paid) {
-  /* Given pointers to salary, amount of tax paid and tax_rate,
-   
+float TAXRATES[] = {0, 20, 40, 45};
+float SALARY_LIMITS[] = {0, 11000, 43000, 150000};
 
-   */
-
-
+int calc_taxes(float *salary_ptr, float *taxes_paid) {
+  /* Given pointers to salary, amount of tax paid, return 0 if no
+     errors.
+     The value under taxes paid will be incremented 
+     by the value of tax you pay at every bracket
+  */
+  int idx = 0;
+  float salary = *salary_ptr;
+  while (salary > SALARY_LIMITS[idx]) {
+    idx += 1;
+}
+  idx -= 1;
+  for(idx; idx > -1; idx--) {
+    *taxes_paid += TAXRATES[idx]/100*(salary - SALARY_LIMITS[idx]);
+    salary = SALARY_LIMITS[idx];
+}
+  return 0;
 }
 
 
+int salary_stats(float *salary_after_tax) {
+  /* Given a pointer to salary after tax value (annual), 
+     print out weekly and monthly allowance */
+  printf("Salary after tax: £%.2f\n", *salary_after_tax); 
+  printf("Your monthly allowance: £%.2f\n", *salary_after_tax/12);
+  printf("Your weekly allowance: £%.2f\n", *salary_after_tax/52);
+  
+  return 0;
+}
+
 
 int main(int argc, char *argv[]) {
-  float salary;
+  float salary, salary_after_tax;
+  float *salary_after_tax_ptr = &salary_after_tax;
   float *salary_ptr = &salary;  
+  float taxes_paid = 0;
+  float *ptr_taxes_paid = &taxes_paid;
+  int errno;
+
   if (argc == 1) {
     printf("What are you offered? \n");
     fscanf(stdin, "%f", salary_ptr);
@@ -45,6 +76,10 @@ int main(int argc, char *argv[]) {
     char *end;
     *salary_ptr = strtof(number, &end);
 }
-  printf("%f\n", *salary_ptr);
+  // printf("%f\n", *salary_ptr);
+  errno = calc_taxes(salary_ptr, ptr_taxes_paid);
+  *salary_after_tax_ptr = *salary_ptr - *ptr_taxes_paid;
+  // *salary_after_tax_ptr = 25000.0;
+  salary_stats(salary_after_tax_ptr);
   return 0;
 }
