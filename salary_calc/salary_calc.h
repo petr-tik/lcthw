4270efb2abef153cd0b2178bf576cd09ef959f4a
petr-tik <petr-tik@users.noreply.github.com>
2016-07-30 00:14:00 +0100

    rewrote typedefs for countries and taxes

--- salary_calc/salary_calc.h
+++ salary_calc/salary_calc.h
@@ -1,24 +1,31 @@
 #ifndef _SALARY_CALC_H
 #define _SALARY_CALC_H 
 
-enum TAX_REGIME {UK, California, Seattle, NYC, Russia};
+//enum TAX_REGIME {UK, California, Seattle, NYC, Russia};
 
-typedef struct COUNTRY_RULES {
+typedef struct rules_t {
   float TAXRATES[10];
-  float SALARY_LIMITS[10];
-} COUNTRY_RULES;
+  int SALARY_LIMITS[10];
+} rules_t;
 
-struct TAX_REGIMES {
-  COUNTRY_RULES UK = {.TAXRATES = {0, 20, 40, 45}, 
-                      .SALARY_LIMITS = {0, 11000, 43000, 150000}};
-  COUNTRY_RULES California = {.TAXRATES = {1, 2, 4, 6, 8, 9.3, 10.3, 11.3, 12.3},
-                              .SALARY_LIMITS = {0, 7850, 18610, 29372, 40773, 
-                                                51350, 263222, 315866, 526443, 
-                                                1000000};}
-  COUNTRY_RULES Russia = {.TAXRATES = {13},
-                          .SALARY_LIMITS = {100000000};}
 
-} TAX_REGIMES;
+typedef struct country_t {
+  rules_t UK;
+  rules_t California;
+  rules_t Russia;
+} country_t; 
+
+country_t TAX_REGIMES = {
+  .UK = {.TAXRATES = {0, 20, 40, 45}, 
+                             .SALARY_LIMITS = {0, 11000, 43000, 150000}},
+  .California = {.TAXRATES = {1, 2, 4, 6, 8, 
+                                                  9.3, 10.3, 11.3, 12.3},
+                                     .SALARY_LIMITS = {0, 7850, 18610, 29372, 
+                                                       40773, 51350, 263222, 
+                                                       315866, 526443, 1000000}},
+  .Russia = {.TAXRATES = {13},
+                                 .SALARY_LIMITS = {100000000}}
+};
 
 int calc_taxes(float *salary_ptr, float *taxes_paid);
 
