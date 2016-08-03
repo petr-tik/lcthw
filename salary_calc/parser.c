fdb92618c377c2d8599919767e57b71e17147e12
petr-tik <petr-tik@users.noreply.github.com>
2016-08-03 22:34:39 +0100

    added parser methods and func signatures

--- salary_calc/parser.c
+++ salary_calc/parser.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "parser.h"
 
 /* Given a string of chars from the command line, return a struct with flags filled in 
 allowed options:
@@ -10,10 +10,30 @@ allowed options:
    -m married (default 0)
 */ 
 
-typedef struct options_t {
-  float amount;
-  int married;
-  char country;
+
+void options_init(struct options_t *options) {
+  /* Given a ptr to options struct, initialise it with default values */
+  options->amount = 35000;
+  options->married = 0; //won't change unless -m option
+  options->location = 0;
+}
+
+int parse_amount(struct options_t *options, const char delim) {
+/* Given a pointer to options struct and the delimiter, run the strtok */
+  char *token = strtok(NULL, delim);
+  float res = strtof(token); // if no conversion, returns 0
+  if (res == 0) {
+    return 1;
+  } else {
+  options->amount = strtof(token);
+  return 0;
+} // end else
+}
+
+int parse_location(struct options_t *options, const char delim) {
+  /* Similar to parse_amount, takes a options_t ptr and delimiter, to fill the struct with location value */
+  char *token = strtok(NULL, delim);
+  short int = strtoi
 }
 
 int parser(char *arguments, struct options_t *options) {
--- salary_calc/parser.h
+++ salary_calc/parser.h
@@ -4,9 +4,13 @@
 typedef struct options_t {
   float amount;
   int married;
-  char country;
+  short int location;
 } options_t;
 
 int parser(char *arguments, struct options_t *options);
+
+int parse_amount(struct options_t *options, const char delim);
+
+int parse_location(struct options_t *options, const char delim);
 
 #endif
