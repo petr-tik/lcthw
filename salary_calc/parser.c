07c2d8bb18e4b6d0273e9443efce8d39dfe82ddb
petr-tik <petr-tik@users.noreply.github.com>
2016-08-05 01:22:13 +0100

    added a print method

--- salary_calc/parser.c
+++ salary_calc/parser.c
@@ -3,13 +3,15 @@
 #include <string.h>
 #include "parser.h"
 
+// testing, run
+// gcc parser.c -o test && ./test
+
 /* Given a string of chars from the command line, return a struct with flags filled in 
 allowed options:
    -a <amount>
    -l <place> [uk | NYC | russia | cali(fornia) | sea(ttle)
    -m married (default 0)
 */ 
-
 
 void options_init(struct options_t *options) {
   /* Given a ptr to options struct, initialise it with default values */
@@ -18,7 +20,16 @@ void options_init(struct options_t *options) {
   options->location = 0;
 }
 
-int parse_amount(struct options_t *options, const char delim) {
+void print_options(struct options_t *options){
+  printf("Salary is: %f\n", options->amount);
+  if (options->married) {
+    printf("Not married\n");
+} else {
+    printf("married\n"); }
+  printf("The location code is: %d\n", options->location);
+}
+
+int parse_amount(struct options_t *options, const char *delim) {
 /* Given a pointer to options struct and the delimiter, run the strtok */
   char *token = strtok(NULL, delim);
   float res = strtof(token); // if no conversion, returns 0
@@ -31,7 +42,7 @@ int parse_amount(struct options_t *options, const char delim) {
 
 } // end func
 
-int parse_location(struct options_t *options, const char delim) {
+int parse_location(struct options_t *options, const char *delim) {
   /* Similar to parse_amount, takes a options_t ptr and delimiter, to fill the struct with location value */
   char *token = strtok(NULL, delim);
   short int loc = atoi(token);
@@ -43,7 +54,7 @@ int parse_location(struct options_t *options, const char delim) {
 int parser(char *arguments, struct options_t *options) {
 /* Given pointers: to arg string and to options struct, return errcode and modiy the struct at pointer */
   char *token;
-  const char delim = " ";
+  const char *delim = " ";
   token = strtok(arguments, delim);
   while(token != NULL) {
     token = strtok(arguments, delim);
@@ -58,3 +69,11 @@ int parser(char *arguments, struct options_t *options) {
 }
 } // while end
 } // parser end
+
+int main(int argc, char *argv[]) {
+// testing it out
+  options_t *args;
+  options_init(args);
+  print_options(args);
+  return 0;
+}
--- salary_calc/parser.h
+++ salary_calc/parser.h
@@ -7,10 +7,14 @@ typedef struct options_t {
   short int location;
 } options_t;
 
+void options_init(struct options_t *options);
+
+void print_options(struct options_t *options);
+
 int parser(char *arguments, struct options_t *options);
 
-int parse_amount(struct options_t *options, const char delim);
+int parse_amount(struct options_t *options, const char *delim);
 
-int parse_location(struct options_t *options, const char delim);
+int parse_location(struct options_t *options, const char *delim);
 
 #endif
