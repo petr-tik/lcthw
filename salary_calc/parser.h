c911c98423fabe74aeb7f9dad66f2492eb0a00f3
petr-tik <petr-tik@users.noreply.github.com>
2016-08-07 23:44:54 +0100

    new func defs + comments in struct def

--- salary_calc/parser.h
+++ salary_calc/parser.h
@@ -1,24 +1,25 @@
 #ifndef _PARSER_H_
 #define _PARSER_H 
 
+void show_version();
+
+void show_help();
+
 typedef struct options_t {
-  float amount;
-  float stock_amount;
-  int married;
-  char stock_quote;
-  short int location;
+  float amount; // mandatory
+  float stock_amount; // optional 
+  int married; // optional 
+  char stock_quote[6]; // optional 
+  short int location; // mandatory
 } options_t;
+
+int check_options(struct options_t *options);
 
 void options_init(struct options_t *options);
 
 void print_options(struct options_t *options);
 
-int parser(int argc, char *argv[], struct options_t *options);
+int parser(int argc, char args[argc], struct options_t *options);
 
-int parse_amount(struct options_t *options, const char *delim);
-
-int parse_location(struct options_t *options, const char *delim);
-
-int parse_shares(struct options_t *options, const char *delim);
 
 #endif
