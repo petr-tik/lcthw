6e5ed47afd8e9798816136f11a7e542f4d4a25ea
petr-tik <petr-tik@users.noreply.github.com>
2016-12-11 00:03:02 +0000

    clang-format

--- data_structures.c
+++ data_structures.c
@@ -8,162 +8,172 @@
 // ./data_structures
 
 /* LinkedList: structs and forward declarations
-Methods include: 
+Methods include:
     add
     search
     delete
-*/ 
+*/
 
 typedef struct Node {
-  /* This LinkedList node stores a int data and a ptr to the next Node struct */ 
-  int data;
-  struct Node * next;
+	/* This LinkedList node stores a int data and a ptr to the next Node
+	 * struct */
+	int data;
+	struct Node *next;
 } Node;
 
-
 typedef struct LinkedList {
-  Node * head;
-  int length;
+	Node *head;
+	int length;
 } LinkedList;
 
-
-Node * create_node(int value) {
-  /* Given a value, create a LinkedList node with that value and 
-     return the pointer to it */ 
-  Node * ll_ptr = (Node *)malloc(sizeof(struct Node));
-  ll_ptr->data = value;
-  ll_ptr->next = NULL;
-  return ll_ptr;
+Node *create_node(int value)
+{
+	/* Given a value, create a LinkedList node with that value and
+	   return the pointer to it */
+	Node *ll_ptr = (Node *)malloc(sizeof(struct Node));
+	ll_ptr->data = value;
+	ll_ptr->next = NULL;
+	return ll_ptr;
 }
 
-int LinkedList_add(LinkedList *list_ptr, int value) {
-  /* Function that adds an element to the singly-linked list
+int LinkedList_add(LinkedList *list_ptr, int value)
+{
+	/* Function that adds an element to the singly-linked list
 
-   */
-  if (list_ptr->head == NULL) {
-    // if the list is empty
-    Node * new_node_ptr = create_node(value);
-    list_ptr->head = new_node_ptr;
-    list_ptr->length = 1;
-    return 0; 
-  } else {
-    Node * node = list_ptr->head;
-    while (node->next != NULL) {
-      node = node->next;
-    } 
-    node->next = create_node(value);
-    list_ptr->length++;
-}  
-  return 0;
-}
-
-
-int LinkedList_search(LinkedList *list_ptr, int value_to_find) {
-  /* Returns 0, if value_to_find is in the list
-             1, if value_to_find is not in the list
-             -1, if list is empty
-   */
-  if (list_ptr->head == NULL) printf("Empty list\n"); return -1;
-  Node * node = list_ptr->head;
-  while (node->next != NULL || node->data == value_to_find) {  
-    if (node->data == value_to_find) {
-      printf("Found %d!\n", value_to_find);
-      return 0;
-}
-    node = node->next;
-}
-  // get to the end and never find the value
-  printf("%d is NOT in the list\n", value_to_find);
-  return 1;
+	 */
+	if (list_ptr->head == NULL) {
+		// if the list is empty
+		Node *new_node_ptr = create_node(value);
+		list_ptr->head = new_node_ptr;
+		list_ptr->length = 1;
+		return 0;
+	} else {
+		Node *node = list_ptr->head;
+		while (node->next != NULL) {
+			node = node->next;
+		}
+		node->next = create_node(value);
+		(list_ptr->length)++;
+	}
+	return 0;
 }
 
-int LinkedList_free_all(LinkedList *list_ptr) {
-  /* Given a pointer to a LinkedList, iterate over all nodes and free them */
-  Node * node = list_ptr->head;
-  while(node != NULL) {
-    Node * temp_node = node->next;
-    free(node);
-    node = temp_node;
-}
-  return 0;
-}
-
-int LinkedList_delete(LinkedList *list_ptr, int value_to_delete) {
-  /* Deletes the first occurence of target value 
-     from the LinkedList and returns 0.
-     If list empty - returns -1;
-     if the value isn't in the list, return 1 */
-  Node * cur = list_ptr->head;
-  if (cur == NULL) return -1;
-  if (value_to_delete == cur->data && list_ptr->length == 1) {
-    int errno = LinkedList_free_all(list_ptr);
-    list_ptr->head = NULL;
-    list_ptr->length == 0;
-    return 0;
-}
-  Node * prev;
-  while(cur->next != NULL) {
-    if (cur->data != value_to_delete) {
-      prev = cur;
-      cur = cur->next;
-}
-    prev->next = cur->next;
-    list_ptr->length--;
-    free(cur);
-    return 0;
-}
-  // if hasn't returned yet, no occurence is in the list
-  return 1;
+int LinkedList_search(LinkedList *list_ptr, int value_to_find)
+{
+	/* Returns 0, if value_to_find is in the list
+		   1, if value_to_find is not in the list
+		   -1, if list is empty
+	 */
+	if (list_ptr->head == NULL)
+		printf("Empty list\n");
+	return -1;
+	Node *node = list_ptr->head;
+	while (node->next != NULL) {
+		if (node->data == value_to_find) {
+			printf("Found %d!\n", value_to_find);
+			return 0;
+		}
+		node = node->next;
+	}
+	// get to the end and never find the value
+	printf("%d is NOT in the list\n", value_to_find);
+	return 1;
 }
 
-void LinkedList_print(LinkedList *list_ptr) {
-  /* Given a pointer to a LinkedList, print each node's value and 
-     return nothing*/
-  Node * node = list_ptr->head;
-  while(node != NULL) {
-    printf("%d\t", node->data);
-    node = node->next;
-}
-  printf("\n");
-}
+int LinkedList_free_all(LinkedList *list_ptr)
+{
+	/* Given a pointer to a LinkedList, iterate over all nodes and free them
+	 */
+	Node *node = list_ptr->head;
+	Node *node_to_delete;
 
-
-int test_LinkedList(void) {
-  /* Function to test LinkedList methods
-     Creates a LinkedList, and tests different functions 
-     by their return vals
-  */
-  LinkedList llist;
-  LinkedList * llist_ptr = &llist;
-  int errno;
-  errno = LinkedList_search(llist_ptr, 8);
-  assert(errno == -1);
-  errno = LinkedList_add(llist_ptr, 5);
-  assert(errno == 0);
-  errno = LinkedList_add(llist_ptr, 7);
-  assert(errno == 0);
-  errno = LinkedList_add(llist_ptr, 8);
-  assert(errno == 0);
-  errno = LinkedList_search(llist_ptr, 5);
-  LinkedList_print(llist_ptr);
-  errno = LinkedList_delete(llist_ptr, 7);
-  assert(errno == 0);
-  LinkedList_print(llist_ptr);
-  //errno = LinkedList_search(llist_ptr, 6);
-  //assert(errno == 1);
-  // LinkedList_free_all(llist_ptr);
+	while (node != NULL) {
+		Node *temp_node = node->next;
+		free(node);
+		node = temp_node;
+	}
+	return 0;
 }
 
+int LinkedList_delete(LinkedList *list_ptr, int value_to_delete)
+{
+	/* Deletes the first occurence of target value
+	   from the LinkedList and returns 0.
+	   If list empty - returns -1;
+	   if the value isn't in the list, return 1 */
+	Node *cur = list_ptr->head;
+	if (cur == NULL)
+		return -1;
+	if (value_to_delete == cur->data && list_ptr->length == 1) {
+		int errno = LinkedList_free_all(list_ptr);
+		list_ptr->head = NULL;
+		list_ptr->length == 0;
+		return 0;
+	}
+	Node *prev;
+	while (cur->next != NULL) {
+		if (cur->data != value_to_delete) {
+			prev = cur;
+			cur = cur->next;
+		}
+		prev->next = cur->next;
+		list_ptr->length--;
+		free(cur);
+		return 0;
+	}
+	// if hasn't returned yet, no occurence is in the list
+	return 1;
+}
 
-/* Binary Search Trees 
+void LinkedList_print(LinkedList *list_ptr)
+{
+	/* Given a pointer to a LinkedList, print each node's value and
+	   return nothing*/
+	Node *node = list_ptr->head;
+	while (node != NULL) {
+		printf("%d\t", node->data);
+		node = node->next;
+	}
+	printf("\n");
+}
+
+int test_LinkedList(void)
+{
+	/* Function to test LinkedList methods
+	   Creates a LinkedList, and tests different functions
+	   by their return vals
+	*/
+	LinkedList llist;
+	LinkedList *llist_ptr = &llist;
+	int errno;
+	errno = LinkedList_search(llist_ptr, 8);
+	assert(errno == -1);
+	errno = LinkedList_add(llist_ptr, 5);
+	assert(errno == 0);
+	errno = LinkedList_add(llist_ptr, 7);
+	assert(errno == 0);
+	errno = LinkedList_add(llist_ptr, 8);
+	assert(errno == 0);
+	errno = LinkedList_search(llist_ptr, 5);
+	LinkedList_print(llist_ptr);
+	errno = LinkedList_delete(llist_ptr, 7);
+	assert(errno == 0);
+	LinkedList_print(llist_ptr);
+	// errno = LinkedList_search(llist_ptr, 6);
+	// assert(errno == 1);
+	// LinkedList_free_all(llist_ptr);
+}
+
+/* Binary Search Trees
    Need a different Node structure with pointers to left and right
    Methods include:
       search
       delete
       insert
 
-All operations take the pointer to the BST as argument and 
-return int for success/failure. Either way, the BST is available under the same pointer
+All operations take the pointer to the BST as argument and
+return int for success/failure. Either way, the BST is available under the same
+pointer
 */
 
 /* typedef struct BSTNode { */
@@ -197,7 +207,8 @@ return int for success/failure. Either way, the BST is available under the same 
 
 /* int BST_insert(int value, BSTNode *tree) */
 /* { */
-/*   /\* Given a value and a pointer to a Binary Search Tree, insert the value */
+/*   /\* Given a value and a pointer to a Binary Search Tree, insert the value
+ */
 /*   return 0 if successful, 1 otherwise. */
 /*   The updated (or not, if return 1) tree */
 /*   will be available at the same pointer address *\/ */
@@ -215,33 +226,35 @@ return int for success/failure. Either way, the BST is available under the same 
 /*   return 1; // if it ever gets here, it never inserted a node */
 /* } */
 
-int get_option() {
-  /* Prints a numbered list of options and returns the chosen value,
-     restarts if the value isn't allowed */
-  int answer;
-  printf("Which simulation do you want to run\n1. Linked List\n2. Binary Search Tree\n");
-  fscanf(stdin, "%d", &answer);
-  if (answer > 3) {
-    printf("ERROR! Invalid option, try again.\n");
-    get_option();
-} else {
-    return answer;
-}
+int get_option()
+{
+	/* Prints a numbered list of options and returns the chosen value,
+	   restarts if the value isn't allowed */
+	int answer;
+	printf("Which simulation do you want to run\n1. Linked List\n2. Binary "
+	       "Search Tree\n");
+	fscanf(stdin, "%d", &answer);
+	if (answer > 3) {
+		printf("ERROR! Invalid option, try again.\n");
+		get_option();
+	} else {
+		return answer;
+	}
 }
 
 int main(int argc, char *argv[])
 {
-  switch(get_option()) {
-    case 1:
-      test_LinkedList();
-      return 0;
-    case 2:
-      //test_BST();
-      return 0;
+	switch (get_option()) {
+	case 1:
+		test_LinkedList();
+		return 0;
+	case 2:
+		// test_BST();
+		return 0;
 
-    default:
-      return 1;
+	default:
+		return 1;
 
-  return 0;
-}
+		return 0;
+	}
 }
