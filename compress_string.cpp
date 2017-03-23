b9faa0da74bc42d5ec2728f44939615c54135d20
Jonathan B Coe <jbcoe@users.noreply.github.com>
2017-03-23 23:50:08 +0000

    Optimised compress method
    remove namespace std
    
    uses a char and a counter int variable instead of array of letters and counters. Similar to a stack.

--- compress_string.cpp
+++ compress_string.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <vector>
-#include <string>
 #include <sstream>
+#include <string>
+#include <vector>
 
 /*
   Given a pointer to a string to compress
@@ -9,53 +9,46 @@
   <char><char_count> of all the characters in the to_compress string
   eg. aaabbcccdff -> a3b2c3d1f2
 
+
   clang++-3.7 -std=c++11 compress_string.cpp -o compress_string
   ./compress_string <string_to_compress>
 */
 
-using namespace std;
-
-string compress(const string &to_compress)
+std::string compress(const std::string &to_compress)
 {
-	/*
-	  Takes a reference (not pointer) to a const string, which makes the
-	  string immutable. Creates a counter (init at 0) and a stringstream,
-	  which is initialised with the first character of the string. After
-	  that, iterate over the given string compare it to the last char in the
-	  stringstream. If the char is different, append the counter of the
-	  previous char, reset counter to 1 and append the new char.
+	int count = 0;
+	char letter = 0; // not a letter
 
-	 */
-	stringstream ss;
-	int counter = 0;
-	ss << to_compress.at(0);
+	std::stringstream ss;
 	for (char c : to_compress) {
-		if (ss.str().back() == c) {
-			++counter;
+		if (c == letter) {
+			++count;
 		} else {
-			// new element - add to letters and set its count to 1
-			ss << counter;
-			counter = 1;
+			if (count > 1) {
+				ss << count;
+			}
+			letter = c;
+			count = 1;
 			ss << c;
 		}
 	}
-
+	ss << count; // add the count for the last char
 	return ss.str();
 }
 
 int main(int argc, char *argv[])
 {
-	string to_compress;
+	std::string to_compress;
 	if (argc == 2) {
 		to_compress = argv[1];
-
+		std::cout << "let's compress some shizz\n";
 	} else {
-		cout << "ERROR. Only 2 arguments accepted" << endl;
+		std::cerr << "ERROR. Only 2 arguments accepted\n";
 		return 1;
 	}
 
-	string res = compress(to_compress);
-	cout << res << endl;
-
+  	std::string res = compress(to_compress);
+  	std::cout << res << '\n';
 	return 0;
 }
+
