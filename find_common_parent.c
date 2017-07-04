bc5428db26ff1e99b86c88b61b2ebad9b8464216
petr-tik <petr-tik@users.noreply.github.com>
2017-07-05 00:36:58 +0100

    Method to make a test tree

--- find_common_parent.c
+++ find_common_parent.c
@@ -7,6 +7,15 @@ Given a root of a binary search tree and 2 values.
 Return the value of the ancestor between the 2 values. If they don't share an
 ancestor, return null
 
+Normal compilation:
+
+$ clang find_common_parent.c -o find_common_parent
+
+with gdb
+$ clang -g find_common_parent.c -o find_common_parent
+
+
+
 */
 
 // definitions
@@ -89,17 +98,25 @@ node_t *make_tree()
 	return root;
 }
 
-int make_tree_of(int elements)
+node_t *make_tree_manual()
 {
-	// given the number of elements, create a tree with the number
-	// of
-	// elements and return a pointer to root
-	// TODO
-	return 1;
+	node_t *root = create_node(20);
+	int res;
+	node_t *first_r_child = create_node(10);
+	first_r_child->right_child = create_node(5);
+	first_r_child->left_child = create_node(15);
+	root->right_child = first_r_child;
+
+	node_t *first_l_child = create_node(30);
+	first_l_child->left_child = create_node(35);
+	first_l_child->right_child = create_node(25);
+	root->left_child = first_l_child;
+	return root;
 }
 
 int main(int argc, char *argv[])
 {
 	printf("Hello world\n");
+	node_t *root = make_tree_manual();
 	return 0;
 }
