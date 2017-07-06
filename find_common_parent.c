dad95f6af26e1e1c421ff26bfd362b55c2d58d57
petr-tik <petr-tik@users.noreply.github.com>
2017-07-06 23:56:01 +0100

    new method to check if nodes in tree

--- find_common_parent.c
+++ find_common_parent.c
@@ -14,13 +14,13 @@ $ clang find_common_parent.c -o find_common_parent
 with gdb
 $ clang -g find_common_parent.c -o find_common_parent
 
-
-
 */
 
 // definitions
 
 int generate_random_between_l_n_r(int l, int r);
+
+// node struct and typedef
 
 typedef struct node {
 	int value;
@@ -53,10 +53,52 @@ node_t *make_tree_manual()
 	return root;
 }
 
+int is_value_in_tree(node_t *root, int val)
+{
+	// given a value, return 1 if a node with such value is in the tree.
+	// Else return 1
+	node_t *cur = root;
+	while (cur) {
+		if (val == cur->value) {
+			return 1;
+		} else if (val > cur->value) {
+			cur = cur->left_child;
+		} else {
+			cur = cur->right_child;
+		}
+	}
+	return 0;
+}
+
+node_t *find_common_ancestor(node_t *root, int val1, int val2)
+{
+	node_t *ret = NULL;
+	node_t *cur = root;
+	while (cur) {
+		if (((val1 < cur->value) && (val2 > cur->value)) ||
+		    ((val1 > cur->value) && (val2 < cur->value))) {
+			if (is_value_in_tree(cur, val1) &&
+			    is_value_in_tree(cur, val2) == 1) {
+				return cur;
+			}
+		} else if (val1 < cur->value && cur->value < val2) {
+			cur = cur->right_child;
+		} else if (val1 > cur->value && cur->value > val2) {
+			cur = cur->left_child;
+		}
+	}
+	return ret;
+}
+
 int main(int argc, char *argv[])
 {
-	printf("Hello world\n");
 	node_t *root = make_tree_manual();
+	node_t *res = find_common_ancestor(root, 37, 25);
+	if (res) {
+		printf("%d\n", res->value);
+		return 0;
+	}
+	printf("%p", res);
 
 	return 0;
 }
