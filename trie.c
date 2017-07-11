f4c5e9888c1974b91a55c19e227dffa70dd07769
petr-tik <petr-tik@users.noreply.github.com>
2017-07-11 22:54:20 +0100

    Implemented the recursive print method
    Builds an array of pointers as path through the trie. If a node makes a
    word, prints it, otherwise recursively calls itself on all its children.

--- trie.c
+++ trie.c
@@ -9,7 +9,7 @@ Supports English alphabet, hence number of letters
 
 */
 
-#define LETTERS 26
+#define LETTERS 26 // english by default
 
 typedef struct trie_node {
 	struct trie_node *children[LETTERS];
@@ -26,9 +26,15 @@ int char_to_alph_index(char letter)
 
 trie_node_t *add_node(char letter)
 {
-	// called when children[idx] returns a NULL pointer
+	/*
+	   called when children[idx] returns a NULL pointer
+	   uses the recursive property of add_to_trie. If it fails and returns a
+	   NULL pointer, it will be written to the relevant index at
+	   node->children[] and the recursive call will try again.
+	*/
 	trie_node_t *new_node = malloc(sizeof(trie_node_t));
 	if (new_node) {
+		// if alloc fails, cannot access NULL pointer
 		new_node->letter = letter;
 	}
 	return new_node;
@@ -52,12 +58,21 @@ trie_node_t *add_to_trie(trie_node_t *root, const char *word, int idx_in_word)
 	return root;
 }
 
-void print_simple_trie(trie_node_t *root)
+void print_simple_trie(trie_node_t *root, trie_node_t *path_so_far[], int idx)
 {
-	printf("%c", root->letter);
-	for (int idx = 0; idx < LETTERS; idx++) {
-		if (root->children[idx]) {
-			print_simple_trie(root->children[idx]);
+	path_so_far[idx] = root;
+	if (root->is_word == 1) {
+		// start from 1 to skip the H head node
+		for (int path_idx = 1; path_idx <= idx; path_idx++) {
+			printf("%c", path_so_far[path_idx]->letter);
+		}
+		printf("\n");
+	}
+	for (int child_idx = 0; child_idx < LETTERS; child_idx++) {
+		if (root->children[child_idx]) {
+			int new_idx = idx + 1;
+			print_simple_trie(root->children[child_idx],
+					  path_so_far, new_idx);
 		}
 	}
 }
@@ -66,7 +81,10 @@ int main(int argc, char *argv[])
 {
 	trie_node_t *head = add_node('H');
 	const char *word = "bzed";
+	const char *word_two = "bzea";
 	trie_node_t *res = add_to_trie(head, word, 0);
-	print_simple_trie(res);
+	trie_node_t *res_two = add_to_trie(head, word_two, 0);
+	trie_node_t *path[10];
+	print_simple_trie(res, path, 0);
 	return 0;
 }
